675addf01b733fdc57fbd46c868cff735cabe913
Dandara Vaz <dandarapatriciovaz@gmail.com>
2023-11-29 11:58:06 -0300

    wordsList

--- src/data/words.jsx
+++ src/data/words.jsx
@@ -1,3 +1,8 @@
 export const wordsList = {
     carro:["Motor", "Volante", "Pneu", "Porta", "Capô"],
+    corpo:["Braço", "Perna", "Cabeça", "Coração", "Fêmur"],
+    fruta:["Banana", "Uva", "Goiaba", "Manga", "Acerola"],
+    país:["Peru", "China", "Canadá", "Índia", "Madagascar"],
+    comida:["Salada", "Feijão", "Bolo", "Milho", "Sopa"],
+    programação: ["React", "JavaScript", "Linguagem", "Framework", "CSS"],
 };
